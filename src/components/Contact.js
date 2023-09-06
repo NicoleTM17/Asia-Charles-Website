4869b7a6816d3a21996b9443edfd91eef392fb93
Nicole Moncrieffe <nicoletmoncrieffe@gmail.com>
2023-09-06 12:59:36 +0100

    completely restructured contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@
 import '../styles/Contact.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+// import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 // import { faChevronUp} from '@fortawesome/free-solid-svg-icons';
 
@@ -12,33 +12,36 @@ function Contact() {
   return(
     <div id='contact'>
 
-    {/* <Link className='chevron-btn sticky' to='#home' smooth><FontAwesomeIcon icon={faChevronUp}/></Link> */}
-
-      <div className='contact-me'>
-
-        <div className='contact-details'>
-          <h1 className='connect-header'>Connect with me</h1>
-          {/* <p style={{textAlign: 'center', fontSize: '2em'}}>‧₊˚❀༉‧₊˚.</p> */}
-          <div className='enquiries'>
-            <h2 className='business-enquiries'>Business enquiries:</h2>
-            <p className='email'>Email address: asicharles.mindfulness@gmail.com </p>
-          </div>
-
-            <a href='https://www.instagram.com/gypsysoulsnippet/' target='_blank' rel="noreferrer"><span className='insta'><FontAwesomeIcon icon={faInstagram}/></span></a>
-            <div className='youtube'><a href='https://www.youtube.com/@justlovemeditations' target='_blank' rel="noreferrer"><span className='yt'><FontAwesomeIcon icon={faYoutube}/></span></a><span style={{fontStyle: 'italic',fontSize: '0.75em'}}>Subscribe to my channel/Listen to my guided meditations</span></div>
-
-        </div>
+    <div className='contact-wrapper'>
 
 
-        <div className='form-section' style={{marginRight: '-7em'}}>
-          <Form/>
+    <div className='contact-details'>
+      <h1 className='contact-header'>Contact and bookings</h1>
+      <p>Get in touch with me today to discuss...</p>
+      <ul className='discussion-list'>
+        <li>Bookings and Consultations</li>
+        <li>Questions about services</li>
+        <li>Questions about pricing</li>
+        <li>General enquiries</li>
+      </ul>
+      <p>Email by filling in this form - I will try to get back to you within 2-4 working days </p>
+      <p>Or directly send an email to <span style={{textDecoration: 'underline', fontWeight: 'bold'}}>asicharles.mindfulness@gmail.com</span></p>
 
-        </div>
+      <div className='youtube'>Subscribe to my <a style={{color: 'var(--nav-color)'}} href='https://www.youtube.com/@justlovemeditations' target='_blank' rel="noreferrer"><FontAwesomeIcon style={{marginRight: '.2em'}} icon={faYoutube}/></a>channel to check out my guided meditations </div>
+      <span style={{marginRight: '.2em'}}>↳</span><a style={{color: 'var(--nav-color)', fontWeight: 'bold'}} href='https://www.youtube.com/@justlovemeditations' target='_blank' rel="noreferrer"><span>Just Love Meditations</span></a>
+
+    </div>
 
 
-      </div>
+
+    <div className='contact-form'>
+      <Form/>
+
+    </div>
 
 
+
+    </div>
     </div>
   )
 };
